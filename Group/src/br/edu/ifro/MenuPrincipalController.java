0d0e3632baf48051df440fcd81f8889c23a6910c
04154521209 <04154521209@JIPA-LABIN05-26.ifro.local>
2018-05-07 09:00:43 -0400

    calculadora

--- Group/src/br/edu/ifro/MenuPrincipalController.java
+++ Group/src/br/edu/ifro/MenuPrincipalController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,12 +21,21 @@ import javafx.scene.control.Label;
 public class MenuPrincipalController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField txtnumero1, txtnumero2;
+    @FXML
+    private Label lblresultado;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button btnsomar;
+    
+    @FXML
+    private void soma(ActionEvent event) {
+       Double num1 = Double.parseDouble(txtnumero1.getText());
+       Double num2 = Double.parseDouble(txtnumero2.getText());
+       Double resultado = num1 + num2;
+       lblresultado.setText(resultado.toString());
+
+
     }
     
     @Override
