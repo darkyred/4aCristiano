55fe6388fc39b4a8af0a10f7ee6564c099a58f63
04154521209 <04154521209@JIPA-LABIN05-26.ifro.local>
2018-05-14 08:34:09 -0400

    Criando alunos e o btn salvar e fechar

--- Group/src/br/edu/ifro/AlunoController.java
+++ Group/src/br/edu/ifro/AlunoController.java
@@ -5,9 +5,17 @@
  */
 package br.edu.ifro;
 
+import br.edu.ifro.modelo.Aluno;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * FXML Controller class
@@ -16,6 +24,9 @@ import javafx.fxml.Initializable;
  */
 public class AlunoController implements Initializable {
 
+    @FXML
+    private JFXTextField txtnome;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +34,22 @@ public class AlunoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void salvar(ActionEvent event) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula");
+        EntityManager em = emf.createEntityManager();
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome(txtnome.getText());
+        em.getTransaction().begin();
+        em.persist(em);
+        em.getTransaction().commit();
+    }
+
+    @FXML
+    private void fechar(ActionEvent event) {
+    Stage stage = (Stage) txtnome.getScene().getWindow();
+    stage.close();
+    }
     
 }
